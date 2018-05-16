037491a416c5565ce2e9a69c92a023671d1efc20
Javier Kipen <jkipen@itba.edu.ar>
2018-05-16 00:32:43 -0300

    Código para probar cuantas iteraciones en promedio necesita

--- EDABitcoins/EDABitcoins/main.cpp
+++ EDABitcoins/EDABitcoins/main.cpp
@@ -1,14 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <time.h>
 #include "picosha2.h"
 
+using namespace std;
+using namespace picosha2;
+
+#define MAX_NMBR_OF_CEROS_ON_HASH 5
+#define RUNS_PER_CERO 10
 
 void main(void)
 {
-	std::string src_str = "The quick brown fox jumps over the lazy dog";
-
-	std::vector<unsigned char> hash(picosha2::k_digest_size);
-	picosha2::hash256(src_str.begin(), src_str.end(), hash.begin(), hash.end());
+	srand(time(NULL));
+	string src_str = "The quick brown fox jumps over the lazy dog";
+	vector<unsigned char> hash(k_digest_size);
+	vector<float> avgIterations(MAX_NMBR_OF_CEROS_ON_HASH);
+	for (unsigned int i = 1; i < MAX_NMBR_OF_CEROS_ON_HASH; i++) //Para cada cantidad de ceros
+	{
+		for (unsigned int j = 0; j < RUNS_PER_CERO; j++) //Se corren muchas veces
+		{
+			unsigned int k = 0;
+			bool exit = false;
+			string aux = "The quick brown fox jumps over the lazy dog" + to_string(rand());
+			while (!exit)
+			{
+				k++;
+				string aux2 = aux + to_string(k);
+				hash256(aux2.begin(), aux2.end(), hash.begin(), hash.end());
+				for (unsigned int l = 0; l < i; l++)
+				{
+					if (l == i - 1 && hash[l] == 0)
+						exit = true;
+					else if (hash[l] != 0)
+						break;
+				}
+				
+			}
+			avgIterations[i] += k;
+		}
+		avgIterations[i] = avgIterations[i] / (float)RUNS_PER_CERO;
+	}
 
 	std::string hex_str = picosha2::bytes_to_hex_string(hash.begin(), hash.end());
 }
